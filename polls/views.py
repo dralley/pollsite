05e4daeb39f8d470fb7c6d3e29112c2a67bd1a87
dralley <dralley@ncsu.edu>
2014-06-12 16:29:07 -0400

    up through Ch. 4

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,23 @@ from django.http import HttpResponse, get_object_or_404
 from polls.models import Poll
 
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+
+    def get_queryset(self):
+        return Poll.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
+
+
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
     context = {'latest_poll_list': latest_poll_list}
@@ -15,8 +32,18 @@ def detail(request, poll_id):
 
 
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s" % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/results.html', {'poll': poll})
 
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s" % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Chioce.DoesNotExist):
+        return render(request, 'polls/detail.html', {'poll': p, 'error_message': "You didn't select a choice"})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,))
+
