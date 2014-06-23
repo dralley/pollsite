130a63fd350a4f8e5da3382c5b1e0aa4a4faf062
dralley <dralley@ncsu.edu>
2014-06-23 13:50:24 -0400

    finished bootstrapping

--- pollsite/settings.py
+++ pollsite/settings.py
@@ -82,4 +82,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
 STATIC_URL = '/static/'
